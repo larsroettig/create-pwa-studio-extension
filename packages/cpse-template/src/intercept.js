bcd42f87f89998e205ac41c4d5e4c446b3bab6bd
Marcin Kwiatkowski <contact@frodigo.com>
2020-10-27 08:07:32 +0100

    Added moduleOverrideWebpackPlugin and componentOverrideMapping files

--- packages/cpse-template/src/intercept.js
+++ packages/cpse-template/src/intercept.js
@@ -4,6 +4,9 @@
  *
  * If do want extend @magento/peregrine or @magento/venia-ui
  * you should add them to peerDependencies to your package.json
+ *
+ * If you want to add overwrites for @magento/venia-ui components you can use
+ * moduleOverrideWebpackPlugin and componentOverrideMapping
  */
 module.exports = targets => {
   targets.of('@magento/pwa-buildpack').specialFeatures.tap(flags => {
