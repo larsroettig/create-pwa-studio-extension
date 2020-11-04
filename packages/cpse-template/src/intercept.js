12440631b2de767a11eed6943681f0bf8c75e160
Cody Nguyễn <cody@simicart.com>
2020-11-04 08:46:54 +0700

    Update buildpack extensions.
    Need to be able to add .graphql file.

--- packages/cpse-template/src/intercept.js
+++ packages/cpse-template/src/intercept.js
@@ -14,6 +14,6 @@ module.exports = targets => {
          *  Wee need to activated esModules and cssModules to allow build pack to load our extension
          * {@link https://magento.github.io/pwa-studio/pwa-buildpack/reference/configure-webpack/#special-flags}.
          */
-        flags[targets.name] = {esModules: true, cssModules: true};
+        flags[targets.name] = { esModules: true, cssModules: true, graphqlQueries: true };
     });
 };
